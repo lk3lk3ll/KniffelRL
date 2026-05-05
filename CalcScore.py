a37b0bc82cc354918b5c0c39c211058560e85a0e
annak <a@b.c>
2026-05-05 22:25:24 +0200

    CalcScore Funktion

--- CalcScore.py
+++ CalcScore.py
@@ -1,2 +1,132 @@
-def calcScore(self, comb: int, dices: list[int]) -> int:
-    pass
+Ones = 0
+Twos = 1
+Threes = 2
+Fours = 3
+Fives = 4
+Sixes = 5
+ThreeOfKind = 6
+FourOfKind = 7
+FullHouse = 8
+SmallStreet = 9
+BigStreet = 10
+Kniffel = 11
+Chance = 12
+CombinationCount = 13
+
+
+
+
+def sumOf(dices, number):
+    result = 0
+    for i in range(5):
+        if dices[i] == number:
+            result += number
+    if result < 3:
+        result = 0
+    return result
+
+
+
+def totalSum(dices):
+    sum = 0
+    for i in range(5):
+        sum = sum + dices[i]
+    return sum
+
+
+
+def threeOfAKind(dices):
+    if maxCountOfSameKind(dices) >= 3:
+        return totalSum(dices)
+    else:
+        return 0
+
+
+def fourOfAKind(dices):
+    if maxCountOfSameKind(dices) >= 4:
+        return totalSum(dices)
+    else:
+        return 0
+
+
+
+def maxCountOfSameKind(dices):
+    count = 1
+    maxCount = 1
+    for i in range(1, 5):
+        if dices[i] == dices[i-1]:
+            count += 1
+            if maxCount < count:
+                maxCount = count
+        else:
+            count = 1
+    return maxCount
+
+
+def fullHouse(dices):
+    if dices[0] == dices[1] and dices[3] == dices[4]:
+        if (dices[2] == dices[1]) != (dices[2] == dices[3]):
+            return 25
+        else:
+            return 0
+    else:
+        return 0
+
+
+def smallStraight(dices):
+    count = 1
+    for i in range(1, 5):
+        diff = (dices[i] - dices[i-1])
+        if diff == 1:
+            count += 1
+            if count == 4:
+                return 30
+        elif diff >= 2:
+            count = 1
+    return 0
+
+
+
+
+def largeStraight(dices):
+    for i in range(1, 5):
+        diff = (dices[i] - dices[i-1])
+        if diff != 1:
+            return 0
+    else:
+        return 40
+
+
+def kniffel(dices):
+    if maxCountOfSameKind(dices) == 5:
+        return 50
+    else:
+        return 0
+
+
+def chance(dices):
+    return totalSum(dices)
+
+
+def calcScore(comb: int, dices: list[int]) -> int:
+    dices.sort()
+    match comb:
+        case 0 | 1 | 2 | 3 | 4 | 5 :
+            return sumOf(dices, comb + 1);
+        case 6:
+            return threeOfAKind(dices);
+        case 7:
+            return fourOfAKind(dices);
+        case 8:
+            return fullHouse(dices);
+        case 9:
+            return smallStraight(dices);
+        case 10:
+            return largeStraight(dices);
+        case 11:
+            return kniffel(dices);
+        case 12:
+            return chance(dices);
+
+
+
---
+++ TestCalcScore.py
@@ -0,0 +1,53 @@
+import unittest
+from CalcScore import calcScore, Ones, ThreeOfKind, FourOfKind, FullHouse, SmallStreet, BigStreet, Kniffel, Chance
+
+
+class TestCalcScore(unittest.TestCase):
+    def testSumOf(self):
+        self.assertEqual(5, calcScore(Ones, [1, 1, 1, 1, 1]))
+        self.assertEqual(3, calcScore(Ones, [1, 2, 1, 3, 1]))
+        self.assertEqual(0, calcScore(Ones, [1, 3, 1, 3, 2]))
+        self.assertEqual(0, calcScore(Ones, [2, 3, 1, 3, 2]))
+
+    def testThreeOfAKind(self):
+        self.assertEqual(17,calcScore(ThreeOfKind,[5, 5, 5, 1, 1]))
+        self.assertEqual(0,calcScore(ThreeOfKind,[2, 5, 5, 1, 1]))
+        self.assertEqual(15,calcScore(ThreeOfKind,[3, 5, 3, 1, 3]))
+        self.assertEqual(0, calcScore(ThreeOfKind, [3, 5, 2, 1, 4]))
+
+    def testFourOfAKind(self):
+        self.assertEqual(21, calcScore(FourOfKind, [5, 5, 5, 1, 5]))
+        self.assertEqual(0, calcScore(FourOfKind, [2, 5, 5, 1, 1]))
+        self.assertEqual(15, calcScore(FourOfKind, [3, 3, 3, 3, 3]))
+
+    def testFullHouse(self):
+        self.assertEqual(25, calcScore(FullHouse, [2, 5, 5, 2, 5]))
+        self.assertEqual(0, calcScore(FullHouse, [5, 5, 5, 5, 5]))
+        self.assertEqual(0, calcScore(FullHouse, [3, 3, 1, 3, 3]))
+        self.assertEqual(0, calcScore(FullHouse, [3, 3, 1, 2, 2]))
+
+    def testSmallStreet(self):
+        self.assertEqual(30, calcScore(SmallStreet, [4, 3, 5, 2, 4]))
+        self.assertEqual(0, calcScore(SmallStreet, [5, 5, 5, 5, 5]))
+        self.assertEqual(0, calcScore(SmallStreet, [1, 2, 1, 3, 3]))
+        self.assertEqual(0, calcScore(SmallStreet, [3, 3, 1, 2, 2]))
+
+    def testBigStreet(self):
+            self.assertEqual(40, calcScore(BigStreet, [4, 3, 5, 2, 6]))
+            self.assertEqual(0, calcScore(BigStreet, [5, 5, 5, 5, 5]))
+            self.assertEqual(0, calcScore(BigStreet, [1, 1, 2, 3, 4]))
+            self.assertEqual(0, calcScore(BigStreet, [1, 2, 4, 6, 3]))
+            self.assertEqual(0, calcScore(BigStreet, [3, 3, 1, 3, 3]))
+
+    def testKniffel(self):
+        self.assertEqual(50, calcScore(Kniffel, [5, 5, 5, 5, 5]))
+        self.assertEqual(0, calcScore(Kniffel, [2, 5, 5, 1, 1]))
+        self.assertEqual(0, calcScore(Kniffel, [3, 3, 3, 2, 3]))
+
+    def testChance(self):
+        self.assertEqual(21, calcScore(Chance, [5, 5, 5, 1, 5]))
+        self.assertEqual(14, calcScore(Chance, [2, 5, 5, 1, 1]))
+        self.assertEqual(15, calcScore(Chance, [3, 3, 3, 3, 3]))
+
+if __name__ == '__main__':
+    unittest.main()
